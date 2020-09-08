a6d0f35eb4cc1aa31992ff1fcd6b7c9f96e533df
andrey-kupets <61921515+andrey-kupets@users.noreply.github.com>
2020-09-08 21:08:52 +0300

    load with local storage

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './normalize.css';
+import './style.css';
+  
+/*const Header = ({ text }) => <h1 id="hi">{text}</h1>;
+
+const Element = <p>Параграф</p>; 
+
+class Main extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="main">
+        <Header text={'свойство текст' + this.props.count}/>
+        {Element}
+      </div>
+    )
+  }
+}*/
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
